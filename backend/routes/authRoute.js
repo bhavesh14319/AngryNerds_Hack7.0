9848995f504540e7303fc3b06413e40b2387e944
bhavesh <bhavesh.mali052002@gmail.com>
2023-04-02 02:06:00 +0530

    Merge branch 'master' of https://github.com/bhavesh14319/AngryNerds_Hack7.0 into Bhavesh

--- backend/routes/authRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { signup } = require('../controllers/authController');
-const { upload } = require('../middlewares/multer');
-const router = express.Router();
-router.post("/signup", upload.any(), signup);
-// router.post("/signup", signup);
-module.exports = router;
