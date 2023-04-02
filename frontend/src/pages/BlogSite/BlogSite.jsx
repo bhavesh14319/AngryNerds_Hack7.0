a7daa2f49f1598232639728e174797e8eaa5ca24
iamparthvaghela <parthvgla@gmail.com>
2023-04-02 08:26:40 +0530

    Added Blogsite

---
+++ frontend/src/pages/BlogSite/BlogSite.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import wheatimage from '../BlogSite/wheat.jpg'
+import './blogsite.css'
+
+const BlogSite = () => {
+  return (
+    <div className="farmeasy__blogsite">
+        <div className="farmeasy__blogsite-topic">
+            <h1>7 Tips To Get The Best Wheat Produce</h1>
+        </div>
+        <div className="farmeasy__blogsite-img">
+            <img src = {wheatimage}></img>
+        </div>
+        <div className="farmeasy__blogsite-date">
+            <p>26 Feb, 2023</p>
+        </div>
+        <div className="farmeasy__blogsite-content">
+            <p>
+            Wheat is one of the most important food crops in the world and wheat farming requires proper planning to produce a high yield.
+
+Most farmers prefer growing wheat in winter when the average temperature is between 18-24 degree Celsius. However, farmers who reside in extremely cold climates prefer the spring season as it allows enough sunlight for the wheat crop to flourish.
+
+Seven tips for successful wheat seeding and farming for maximum yield:
+
+<br></br>1: Pick a dry area for cultivation: Wheat is both a spring and winter crop, and utilizes at least 8 hours of sun on a daily basis. Therefore, avoid picking a site that’s too shady and doesn’t allow enough sunlight.
+
+<br></br>2: Prepare the land well in advance: Growing wheat requires prepping the soil for strong root growth. Till your soil to a depth of at least 15 cm, and make sure that it’s even.
+
+<br></br>3: Spread the wheat seeds in a semi-circular motion: You can always use a seed spreader too, as long as it spreads the seeds at approximately one seed per 2.5 sq. cm.
+
+<br></br>4: Use phosphorus, nitrogen & Potassium fertilization: Phosphorus and nitrogen collectively help develop strong roots which will help the wheat crop survive winter. Optimum dose of Mahadhan 12:32:16 (75 Kg/ac) can help wheat growers to get more yield. In addition to this, application of Mahadhan Bensulf (10 kg/ac) & Mahadhan Zinc sulphate (10 Kg/ac) can further help wheat farmers to get quality oriented higher yield fetching better market price. However, note that excessive nitrogen fertilization can have adverse effects on the crop’s sustenance.
+
+<br></br>5: Water right after planting the seeds: Winter wheat crops require water as soon as the seeds are planted. It is imperative to keep the entire planting area moist until the plant begins to develop. Then on, follow a strict irrigation schedule.
+
+<br></br>6: Follow an irrigation schedule: For maximum yield of wheat crop, an irrigation schedule of 6 stages post sowing is highly recommended:
+
+<br></br>1st irrigation - 3-4 weeks
+<br></br>2nd irrigation - 40-45 days
+<br></br>3rd irrigation - 60-65 days
+<br></br>4th irrigation - 80-85 days
+<br></br>5th irrigation - 100-105 days
+<br></br>6th irrigation - 105-120 days
+ 
+
+<br></br>7: Use pesticides and insecticides as necessary: Wheat crop is not averse to slugs and insects that can wreak havoc on the plantation, especially when the wheat is still very short. Use chemicals like Chlorpyrifos and acetamiprid in such a case that effectively manage the insects.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default BlogSite
